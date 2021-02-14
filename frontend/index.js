8ba3b82f4cb9dc3cb1b3c255a94980b101492a93
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-14 18:37:04 -0500

    Create canvas element that you can draw on.

---
+++ frontend/index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  console.log('hello!')
+  enableSketch();
+})
+
+function enableSketch(){
+  const canvas = document.getElementById('sketchpad');
+  canvas.height = '600';
+  canvas.width = '400';
+
+  let ctx = canvas.getContext("2d");
+  ctx.fillStyle = 'rgb(235, 213, 179)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  let drawing = false;
+
+  function startDrawing(){
+    drawing = true;
+  }
+
+  function stopDrawing(){
+    drawing = false;
+    ctx.beginPath();
+  }
+
+  function draw(e){
+    if(!drawing) return
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo((e.clientX - canvas.offsetLeft), (e.clientY - canvas.offsetTop));
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+  }
+
+  canvas.addEventListener('mousedown', startDrawing)
+  canvas.addEventListener('mouseup', stopDrawing)
+  canvas.addEventListener('mousemove', draw)
+}
