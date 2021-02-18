b19d3bf8c0013a5436db4c3ba37b206f1e4bc41c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-18 17:57:16 -0500

    Clean up redundant code.

--- frontend/index.js
+++ frontend/index.js
@@ -1,43 +1,6 @@
 document.addEventListener('DOMContentLoaded', () =>{
   console.log('hello!');
-  new Sketchpad(document.getElementById('sketchpad'));
-  Sketchpad.all[0].hide()
-  let index = new Index(document.getElementById('index'));
-  fetch('http:localhost:3000/sketches').then(resp => resp.json()).then(json => index.listSketches(json))
+  let sketchpad = new Sketchpad(document.getElementById('sketchpad'));
+  sketchpad.hide();
+  Sketch.loadIndex();
 })
-
-class Index {
-
-  static all = []
-
-  constructor(element){
-    this.parent = element
-    this.hidden = false
-    // this.hide();
-
-    Index.all.push(this)
-  }
-
-  hide = () => {
-    this.parent.remove()
-    this.hidden = true
-  }
-
-  show = () => {
-    if(!this.hidden) return
-    document.body.appendChild(this.parent)
-    this.hidden = false;
-  }
-
-  listSketches = (array) => {
-    let columns = document.querySelectorAll('div.tile.is-parent.is-vertical')
-    let counter = 0
-    let sketches = Sketch.createCards(array)
-    for(const sketch of sketches){
-      columns[counter % 3].appendChild(sketch)
-      counter++
-    }
-  }
-
-
-}
