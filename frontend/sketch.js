1dc24cef09ceca74f76eea5e0a7f4bce82c4286b
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-18 14:58:49 -0500

    Add static method to construct card elements when given array from fetch request.

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -7,5 +7,22 @@ class Sketch {
     this.image = obj.image;
   }
 
+  static createCards = (array) => {
+    let cards = []
+
+    for(const sketch of array){
+      let card = document.createElement('div');
+      card.classList.add('card', 'is-child', 'box');
+
+      let image = document.createElement('img')
+      image.src = sketch.image;
+
+      card.appendChild(image);
+      cards.push(card)
+    }
+
+    return cards;
+  }
+
 
 }
