89d5ff5c787272d16331409990f14b453ba7ff1d
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-18 13:09:29 -0500

    Build out #create and #index actions.

--- backend/app/controllers/sketches_controller.rb
+++ backend/app/controllers/sketches_controller.rb
@@ -1,2 +1,22 @@
 class SketchesController < ApplicationController
+
+  def create
+    sketch = Sketch.new
+    sketch.image.attach(sketch_params)
+    sketch.save
+
+    render json: sketch, only: [:id, :rating, :image]
+  end
+
+  def index
+    sketches = Sketch.all
+    render json: sketches
+  end
+
+  private
+
+  def sketch_params
+    return params.require(:sketch)
+  end
+
 end
