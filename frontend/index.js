6019a107829f9ceabb71d6d7815cbbef1000852c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-18 14:59:15 -0500

    Index class can render cards on viewport.

--- frontend/index.js
+++ frontend/index.js
@@ -1,44 +1,43 @@
 document.addEventListener('DOMContentLoaded', () =>{
   console.log('hello!');
-  new Sketchpad(document.getElementById('sketchpad'))
+  new Sketchpad(document.getElementById('sketchpad'));
+  Sketchpad.all[0].hide()
+  let index = new Index(document.getElementById('index'));
+  fetch('http:localhost:3000/sketches').then(resp => resp.json()).then(json => index.listSketches(json))
 })
 
 class Index {
 
+  static all = []
+
   constructor(element){
     this.parent = element
+    this.hidden = false
+    // this.hide();
+
+    Index.all.push(this)
   }
 
   hide = () => {
     this.parent.remove()
+    this.hidden = true
+  }
+
+  show = () => {
+    if(!this.hidden) return
+    document.body.appendChild(this.parent)
+    this.hidden = false;
   }
 
   listSketches = (array) => {
-    for(const sketch of array){
-      let mediaObject = createMediaObject()
-      
+    let columns = document.querySelectorAll('div.tile.is-parent.is-vertical')
+    let counter = 0
+    let sketches = Sketch.createCards(array)
+    for(const sketch of sketches){
+      columns[counter % 3].appendChild(sketch)
+      counter++
     }
-
   }
 
-  createMediaObject = () => {
-    let article = document.createElement('article')
-    article.classList.add('media');
-  
-    let mediaLeft = document.createElement('figure')
-    mediaLeft.classList.add('media-left')
-  
-    let mediaContent = document.createElement('div')
-    mediaContent.classList.add('media-content')
-  
-    let mediaRight = document.createElement('div')
-    mediaRight.classList.add('media-right')
-  
-    article.appendChild(mediaLeft)
-    article
-    .appendChild(mediaContent)
-    article.appendChild(mediaRight)
-  
-    return article;
-  }
+
 }
