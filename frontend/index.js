74b8236c43ef00ec76287e91c666dfcd14514ab7
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-18 13:48:23 -0500

    Create class to display page Index.

--- frontend/index.js
+++ frontend/index.js
@@ -2,3 +2,22 @@ document.addEventListener('DOMContentLoaded', () =>{
   console.log('hello!');
   new Sketchpad(document.getElementById('sketchpad'))
 })
+
+class Index {
+
+  constructor(element){
+    this.parent = element
+  }
+
+  hide = () => {
+    this.parent.remove()
+  }
+
+  listSketches = (array) => {
+    for(const sketch of array){
+      let mediaObject = createMediaObject()
+      
+    }
+
+  }
+}
