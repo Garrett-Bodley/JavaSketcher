bd129f4371bd9ed1aa1c4fbd691a721987e298cf
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-18 17:56:51 -0500

    Add methods to display individual Sketch and hide index.

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -1,27 +1,64 @@
 class Sketch {
   static all = [];
+  static showDiv = document.getElementById('show')
+  static index = document.getElementById('index')
+  static hidden = false;
 
   constructor(obj){
     this.id = obj.id;
     this.rating = obj.rating;
     this.image = obj.image;
+
+    Sketch.all.push(this)
   }
 
-  static createCards = (array) => {
-    let cards = []
+  static loadIndex = () => {
+    Sketch.hide()
+    fetch('http:localhost:3000/sketches').then(resp => resp.json()).then(json => Sketch.renderIndex(json))
+  }
 
-    for(const sketch of array){
-      let card = document.createElement('div');
-      card.classList.add('card', 'is-child', 'box');
-
-      let image = document.createElement('img')
-      image.src = sketch.image;
-
-      card.appendChild(image);
-      cards.push(card)
+  static renderIndex = (array) => {
+    let columns = document.querySelectorAll('div.tile.is-parent.is-vertical')
+    let counter = 0
+    for(const element of array){
+      let sketch = new Sketch(element)
+      columns[counter % 3].appendChild(sketch.createCard())
+      counter++
     }
+  }
 
-    return cards;
+  static hideIndex = () => {
+    Sketch.index.remove()
+  }
+
+  createCard = () => {
+    let card = document.createElement('div');
+    card.classList.add('card', 'is-child', 'box');
+
+    let image = document.createElement('img')
+    image.src = this.image;
+    image.style.cursor = 'pointer'
+    image.addEventListener('click', this.display)
+
+    card.appendChild(image);
+    return card;
+  }
+
+  static hide = () => {
+    Sketch.showDiv.remove()
+    Sketch.hidden = true;
+  }
+
+  static show = () => {
+    if(!Sketch.hidden) return
+    document.body.appendChild(Sketch.showDiv);
+    Sketch.hidden = false;
+  }
+
+  display = () => {
+    Sketch.hideIndex()
+    Sketch.show()
+    Sketch.showDiv.querySelector('img').src = this.image;
   }
 
 
