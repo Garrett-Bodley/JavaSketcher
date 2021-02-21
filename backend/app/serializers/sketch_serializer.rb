28ca9fc9228a94056a5439758e1d927773dc9241
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-21 17:20:02 -0500

    Update serializers.

--- backend/app/serializers/sketch_serializer.rb
+++ backend/app/serializers/sketch_serializer.rb
@@ -2,8 +2,10 @@ class SketchSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :rating, :image
+  has_many :comments
 
   def image
     polymorphic_url(object.image) if object.image.attached?
   end
+
 end
